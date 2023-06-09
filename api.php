5c3bcdeaa29719897f6f205c403b21a454d08923
MRKnight <roarijo89@gmail.com>
2023-06-09 19:59:11 -0400

    Update api.php
    read more : https://blogs.cococloud-drive.com/temporary-updates-to-the-api-of-05282023

--- api.php
+++ api.php
@@ -8,7 +8,7 @@ define("key2", "key here 2");
 
 function apiToken() {
   
-  $url = "https://api.cococloud-drive.com/api/v2/authorize"."?key1=".key1."&key2=".key2;
+  $url = "https://cococloud-drive.com/api/v2/authorize"."?key1=".key1."&key2=".key2;
 
   $curl = curl_init();
 
@@ -60,7 +60,7 @@ function uploadFile() {
     'folder_id' => '632'
   );
     
-  $url = "https://api.cococloud-drive.com/api/v2/file/upload";
+  $url = "https://cococloud-drive.com/api/v2/file/upload";
 
   $curl = curl_init();
 
